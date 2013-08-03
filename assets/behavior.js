170ba24587b63e93076ed5d0dc690e8b40304786
AWinterman <andywinterman@gmail.com>
2013-08-03 16:07:21 -0700

    Generated documentation for 2484b1b96e89ae1a59323977d3561fc16ed44bca

--- assets/behavior.js
+++ assets/behavior.js
@@ -31,6 +31,60 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
       },
       "depth": 1,
       "outline": []
+    }, {
+      "type": "file",
+      "data": {
+        "language": {
+          "nameMatchers": [".md"],
+          "commentsOnly": true,
+          "name": "Markdown"
+        },
+        "sourcePath": "/Users/andrewwinterman/dev/d3.force-link-status/README.md",
+        "projectPath": "README.md",
+        "targetPath": "README",
+        "firstHeader": {
+          "type": "heading",
+          "data": {
+            "level": 1,
+            "title": "d3.force-link-status",
+            "slug": "d3force-link-status",
+            "isFileHeader": true
+          },
+          "depth": 1
+        },
+        "title": "d3.force-link-status"
+      },
+      "depth": 1,
+      "outline": [
+        {
+          "type": "heading",
+          "data": {
+            "level": 1,
+            "title": "Example",
+            "slug": "example"
+          },
+          "depth": 1,
+          "children": [
+            {
+              "type": "heading",
+              "data": {
+                "level": 2,
+                "title": "Setup",
+                "slug": "setup"
+              },
+              "depth": 2
+            }
+          ]
+        }, {
+          "type": "heading",
+          "data": {
+            "level": 1,
+            "title": "Checks",
+            "slug": "checks"
+          },
+          "depth": 1
+        }
+      ]
     }
   ];
 
